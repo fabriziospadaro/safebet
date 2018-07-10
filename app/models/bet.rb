6d6600884f7f553720d0772610db32c04c73164d
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-07-10 15:47:54 +0200

    bet system working?

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,9 +1,6 @@
 class Bet < ApplicationRecord
   belongs_to :event
-  belongs_to :team
+  belongs_to :team, optional: true
   has_many :bet_users
   has_many :users, through: :bet_users
-  validates :event, uniqueness: { scope: :users }
-
-  #delegate :unique_event_id, to: :event
 end
