fe732c8746b71cdc168e4ef041948f2ba23c80fb
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-07-18 14:37:46 +0200

    i no push master

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,5 +2,6 @@ class SportsController < ApplicationController
   def index
     @sports = Sport.all
     @parlays = current_user.parlays&.last
+    @error = params[:error] if (params[:error])
   end
 end
