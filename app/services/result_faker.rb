db4c3e975dab1b0adabe50c3a880532a2f7dc39f
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-07-19 18:21:35 +0200

    nice nice

--- app/services/result_faker.rb
+++ app/services/result_faker.rb
@@ -1,6 +1,10 @@
 class ResultFaker
-  def self.call(parlay)
-    new(parlay).fake_results
+  def self.call(parlay,impose_win)
+    if(impose_win)
+      new(parlay).fake_win
+    else
+      new(parlay).fake_results
+    end
   end
 
   def initialize(parlay)
@@ -13,7 +17,43 @@ class ResultFaker
     end
   end
 
+  def fake_win
+    @parlay&.bets.find_each do |b|
+      fake_event_sucess(b)
+    end
+  end
+
   private
+
+  def fake_event_sucess(bet)
+    event = bet.event
+
+    score_a = rand(0..5)
+    score_b = rand(0..5)
+
+    team_a = event.team_a
+    team_b = event.team_b
+    tie = Team.find_by(name: "DRAW")
+
+    score_a = score_b + 1 if(team_a == bet.team && score_a <= score_b)
+
+    score_b = score_a + 1 if(team_a == bet.team && score_b <= score_a)
+
+    if(bet.team == tie )
+      score_a = score_b
+
+    winner = (score_a > score_b) ? team_a : team_b
+    winner = (score_a == score_b) ? tie : winner
+
+    event.update(
+      scraped_score: "#{score_a}-#{score_b}",
+      winner: winner,
+      status: "Finished",
+      team_a_score: score_a,
+      team_b_score: score_b
+    )
+  end
+
 
   def assign_winner_and_score(event)
     score_a = rand(0..5)
